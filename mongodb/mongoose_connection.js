565909cf3ca751dc8bab251677ce2eb4fd8efcb1
Hagil Baik <hagilbaik@gmail.com>
2017-11-30 20:36:34 +0800

    You can drive a horse to water, but a pencil must be lead.

---
+++ mongodb/mongoose_connection.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27500/soojee', {useMongoClient: true});
+
+var recipe_structure = {
+    number: Number,
+    korean_name: String,
+    english_name: String,
+    description: String,
+    ingredients: String,
+    preparation_time: Number,
+    cooking_time: Number,
+    difficulty: Number,
+    instructions: String
+}
+
+var recipe_schema = new mongoose.Schema(recipe_structure, {_id : false});
+
+var RECIPECLASS = mongoose.model('recipe', recipe_structure);
+
+module.exports = RECIPECLASS;
