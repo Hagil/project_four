45690641dfcef9575867e91ff23628ffd45c5abe
Hagil Baik <hagilbaik@gmail.com>
2017-12-04 16:36:28 +0800

    Fifty flippant frogs Walked by on flippered feet And with their slime they made the time Unnaturally fleet.

--- app/routes/ejs_routes.js
+++ app/routes/ejs_routes.js
@@ -35,9 +35,34 @@ function do_homepage(req, res) {
     res.render('index.ejs'); // load the index.ejs file
 }
 
-function do_profile(req, res){
+function do_profile(req, res) {
     console.log('getting profile');
     res.render('profile.ejs');
+}
+
+function do_momSignup(req, res) {
+    console.log('getting mom signup');
+    res.render('auth/mom_signup.ejs');
+}
+
+function do_momLogin(req, res) {
+    console.log('getting mom login');
+    res.render('auth/mom_login.ejs');
+}
+
+function do_guestLogin(req, res) {
+    console.log('getting guest login');
+    res.render('auth/guest_login.ejs');
+}
+
+function do_about(req, res) {
+    console.log('getting about');
+    res.render('misc/about.ejs');
+}
+
+function do_survey(req, res) {
+    console.log('getting survey');
+    res.render('misc/survey.ejs');
 }
 
 function do_recipeGod(req, res) {
@@ -48,4 +73,49 @@ function do_recipeGod(req, res) {
 function do_recipeGuest(req, res) {
     console.log('doing recipe_guest');
     res.render('recipe_lists/recipe_guest.ejs');
+}
+
+function do_bulgogi(req, res){
+    console.log('getting bulgogi');
+    res.render('recipes/bulgogi.ejs');
+}
+function do_dakdoritang(req, res){
+    console.log('getting dakdoritang');
+    res.render('recipes/dakdoritang.ejs');
+}
+function do_japchae(req, res){
+    console.log('getting japchae');
+    res.render('recipes/japchae.ejs');
+}
+function do_kimbap(req, res){
+    console.log('getting kimbap');
+    res.render('recipes/kimbap.ejs');
+}
+function do_kimchiJjigae(req, res){
+    console.log('getting kimchiJjigae');
+    res.render('recipes/kimchi_jjigae.ejs');
+}
+function do_kimchi(req, res){
+    console.log('getting kimchi');
+    res.render('recipes/kimchi.ejs');
+}
+function do_kkaennip(req, res){
+    console.log('getting kkaennip');
+    res.render('recipes/kkaennip.ejs');
+}
+function do_odeng(req, res){
+    console.log('getting odeng');
+    res.render('recipes/odeng.ejs');
+}
+function do_oeeji(req, res){
+    console.log('getting oeeji');
+    res.render('recipes/oeeji.ejs');
+}
+function do_samgaetang(req, res){
+    console.log('getting samgaetang');
+    res.render('recipes/samgaetang.ejs');
+}
+function do_tteokbokki(req, res){
+    console.log('getting tteokbokki');
+    res.render('recipes/tteokbokki.ejs');
 }
