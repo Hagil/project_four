0490ddfb83b8f36828200ba73736c615aa5db425
Hagil Baik <hagilbaik@gmail.com>
2017-12-07 09:29:14 +0800

    In California they don't throw their garbage away -- they make it into television shows. 		-- Woody Allen, "Annie Hall"

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 WDI Web Intensive: Project 4
 Requirements:
-Technologies used: 
+Technologies used: AngularJS, NodeJS, Express, Passport, CSS, Javascript
 Installation: Connect through localhost
 Removal: Disconnect from localhost
---
+++ public/js/survey.js
@@ -0,0 +1,55 @@
+// create our angular app and inject ngAnimate and ui-router 
+// =============================================================================
+angular.module('formApp', ['ngAnimate', 'ui.router'])
+
+// configuring our routes 
+// =============================================================================
+.config(function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+
+        // route to show our basic form (/form)
+        .state('form', {
+            url: '/form',
+            templateUrl: 'misc/form.ejs',
+            controller: 'formController'
+        })
+
+        // nested states 
+        // each of these sections will have their own view
+        // url will be nested (/form/profile)
+        .state('form.profile', {
+            url: '/profile',
+            templateUrl: 'misc/form-profile.ejs'
+        })
+
+        // url will be /form/interests
+        .state('form.favourites', {
+            url: '/favourites',
+            templateUrl: 'misc/form-favourites.ejs'
+        })
+
+        // url will be /form/payment
+        .state('form.thanks', {
+            url: '/thanks',
+            templateUrl: 'partials/form-thanks.ejs'
+        });
+
+    // catch all route
+    // send users to the form page 
+    $urlRouterProvider.otherwise('/form/profile');
+})
+
+// our controller for the form
+// =============================================================================
+.controller('formController', function($scope) {
+
+    // we will store all of our form data in this object
+    $scope.formData = {};
+
+    // function to process the form
+    $scope.processForm = function() {
+        alert('You are awesome!');
+    };
+
+});
